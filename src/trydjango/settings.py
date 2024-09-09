3fcf4b31b308a689fd83d576f9ed1d17307594ce
junaidalam2 <junaid.alam2@gmail.com>
2024-09-08 23:00:12 -0700

    added pages component

--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #third-party
+
+    #own
     'products',
+    'pages',
 ]
 
 MIDDLEWARE = [
